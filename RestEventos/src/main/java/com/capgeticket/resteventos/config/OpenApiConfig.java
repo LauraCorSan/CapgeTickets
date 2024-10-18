dba649fb30debafb7307964eeb3ddfd9502874d2
Cordero Sanchez <laura-nathalia.cordero-sanchez@capgemini.com>
2024-10-18 11:33:42 +0200

    depliegue eureka

---
+++ RestEventos/src/main/java/com/capgeticket/resteventos/config/OpenApiConfig.java
@@ -0,0 +1,17 @@
+package com.capgeticket.resteventos.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+
+@Configuration
+public class OpenApiConfig {
+	
+
+
+}
