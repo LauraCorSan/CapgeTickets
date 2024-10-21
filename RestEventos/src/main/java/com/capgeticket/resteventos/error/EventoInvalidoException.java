01c139089c3db5e39c4fe81a420c8a38989302bc
Gregorio Rosas <laura.gregorio-rosas@capgemini.com>
2024-10-21 16:00:14 +0200

    Added config on remote

--- RestEventos/src/main/java/com/capgeticket/resteventos/error/EventoInvalidoException.java
+++ RestEventos/src/main/java/com/capgeticket/resteventos/error/EventoInvalidoException.java
@@ -11,7 +11,7 @@ public class EventoInvalidoException extends RuntimeException{
 	  private static final long serialVersionUID = 1L;
 
 	    public EventoInvalidoException(String message) {
-	        super(message);
+	        super("{error: " + message + " }");
 	    }
 
 }
