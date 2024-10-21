4a0a2977bffe1bb86dfb074c46e5498571f38e6e
Guillermo <guillerg@LES-5CG12150S3.corp.capgemini.com>
2024-10-21 16:08:42 +0200

    Tests Eventos delete

--- RestEventos/src/test/java/com/capgeticket/resteventos/RestEventosApplicationTests.java
+++ RestEventos/src/test/java/com/capgeticket/resteventos/RestEventosApplicationTests.java
@@ -2,12 +2,53 @@ package com.capgeticket.resteventos;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import java.time.LocalDateTime;
+import java.util.Optional;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
+import com.capgeticket.resteventos.model.Evento;
+import com.capgeticket.resteventos.repository.EventoRepository;
+import com.capgeticket.resteventos.service.EventoServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 
 @SpringBootTest
-class RestEventosApplicationTests {
+public class RestEventosApplicationTests {
 
-	@Test
-	void contextLoads() {
+	 @Mock
+	    private EventoRepository eventoRepository;
+
+	    @InjectMocks
+	    private EventoServiceImpl eventoService;
+
+	    private Evento evento;
+
+	    @BeforeEach
+	    public void setUp() {
+	        evento = new Evento();
+	        evento.setId(1L);
+	        evento.setNombre("Pau");
+	        evento.setDescripcion("concierto anonimo");
+	        evento.setGenero("Rock");
+	        evento.setFecha_evento(LocalDateTime.now().plusDays(1));
+	        evento.setPrecio_min(90.0);
+	        evento.setPrecio_max(5000.0);
+	        evento.setLocalidad("Murcia");
+	        evento.setRecinto("Ayuntamiento");
+	    }
+
+	    @Test
+	    public void eventDeletedSuccess() {
+	        when(eventoRepository.findById(evento.getId())).thenReturn(Optional.of(evento));
+
+	        doNothing().when(eventoRepository).delete(evento);
+
+	        Evento result = eventoService.eliminarEvento(evento);
+
+	        assertEquals(evento.getId(), result.getId());
+	        assertEquals(evento.getNombre(), result.getNombre());
+	    }
+
 	}
-
-}
