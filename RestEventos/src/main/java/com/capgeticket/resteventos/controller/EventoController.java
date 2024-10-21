04510775b977ee239698bb0581979fe60c8d64a5
Gregorio Rosas <laura.gregorio-rosas@capgemini.com>
2024-10-21 12:08:11 +0200

    Added lombok, fixed adapter, added aniadirEvento

--- RestEventos/src/main/java/com/capgeticket/resteventos/controller/EventoController.java
+++ RestEventos/src/main/java/com/capgeticket/resteventos/controller/EventoController.java
@@ -1,10 +1,31 @@
 package com.capgeticket.resteventos.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.capgeticket.resteventos.adapter.EventoAdapter;
+import com.capgeticket.resteventos.model.Evento;
+import com.capgeticket.resteventos.response.EventoResponse;
+import com.capgeticket.resteventos.service.EventoService;
+
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 @RestController
 @RequestMapping("/evento")
 public class EventoController {
+
+	@Autowired
+	private EventoService eventoService;
+
+	@Autowired
+	EventoAdapter eventoAdapter;
+
+	@PostMapping("/aniadir")
+	public EventoResponse aniadirEvento(@RequestBody Evento evento) {
+		Evento e = eventoService.aniadirEvento(evento);
+		return eventoAdapter.toDTO(e);
+	}
 
 }
---
+++ RestEventos/src/main/java/com/capgeticket/resteventos/service/EventoServiceImpl.java
@@ -0,0 +1,23 @@
+package com.capgeticket.resteventos.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.capgeticket.resteventos.adapter.EventoAdapter;
+import com.capgeticket.resteventos.model.Evento;
+import com.capgeticket.resteventos.repository.EventoRepository;
+import com.capgeticket.resteventos.response.EventoResponse;
+
+public class EventoServiceImpl implements EventoService {
+
+	@Autowired
+	private EventoRepository eventoRepository;
+	
+	@Autowired EventoAdapter eventoAdapter;
+
+	@Override
+	public Evento aniadirEvento(Evento evento) {
+		return eventoRepository.save(evento);
+	}
+
+
+}
