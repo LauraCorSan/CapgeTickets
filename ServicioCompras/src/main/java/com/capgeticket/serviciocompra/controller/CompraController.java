6076eff348f6baaa8b31792678e93fae5373fe72
Gregorio Rosas <laura.gregorio-rosas@capgemini.com>
2024-10-24 12:32:14 +0200

    created exceptions classes

---
+++ ServicioCompras/src/main/java/com/capgeticket/serviciocompra/controller/CompraController.java
@@ -0,0 +1,22 @@
+package com.capgeticket.serviciocompra.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * 
+ */
+@RestController
+public class CompraController {
+	
+	
+//	@PostMapping
+//	public CompraConfirmadaResponse nuevaCompra(@RequestBody PeticionCompraResponse peticionCompraResponse) {
+//		if peticionCompraResponse.email is empty throw exception
+//		if peticionCompraResponse.idEvento is empty throw exception
+//		CompraConfirmadaResponse compraConfirmada = compraService.nuevaCompra(peticionCompraResponse)
+//		return compraConfirmada;
+//	}
+
+}
