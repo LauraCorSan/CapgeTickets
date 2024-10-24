fd88f3ce48b904b3f2ec3315baa339bc1ea29c51
Cordero Sanchez <laura-nathalia.cordero-sanchez@capgemini.com>
2024-10-24 12:49:49 +0200

    añadido globalhandlerexception

--- ServicioCompras/src/main/java/com/capgeticket/serviciocompra/error/GlobalExceptionHandler.java
+++ ServicioCompras/src/main/java/com/capgeticket/serviciocompra/error/GlobalExceptionHandler.java
@@ -6,8 +6,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.servlet.NoHandlerFoundException;
-
 
 /**
  * Clase: GlobalExceptionHandler
@@ -28,19 +26,19 @@ public class GlobalExceptionHandler {
 	        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
 	    }
 
-//	    @ExceptionHandler(PeticionCompraIncorrectaException.class)
-//	    public ResponseEntity<ErrorResponse> handleEventoInvalidoException(PeticionCompraIncorrectaException ex) {
-//	        logger.error("Evento inválido: {}", ex.getMessage());
-//	        ErrorResponse errorResponse = new ErrorResponse( HttpStatus.BAD_REQUEST.value(),"La solicitud es incorrecta. Verifica los datos enviados.",ex.getMessage());
-//	        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
-//	    }
+	    @ExceptionHandler(PeticionCompraIncorrectaException.class)
+	    public ResponseEntity<ErrorResponse> handleEventoInvalidoException(PeticionCompraIncorrectaException ex) {
+	        logger.error("Peticion de compra inválida: {}", ex.getMessage());
+	        ErrorResponse errorResponse = new ErrorResponse( HttpStatus.BAD_REQUEST.value(),"La solicitud es incorrecta. Verifica los datos enviados.",ex.getMessage());
+	        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+	    }
 	    
-//	    @ExceptionHandler(ReciboCompraIncorrectaException.class)
-//	    public ResponseEntity<ErrorResponse> handleEventoNotFoundException(ReciboCompraIncorrectaException ex) {
-//	        logger.error("Evento no encontrado: {}", ex.getMessage());
-//	        ErrorResponse errorResponse = new ErrorResponse( HttpStatus.NOT_FOUND.value(),"Evento no encontrado",ex.getMessage());
-//	        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
-//	    }
+	    @ExceptionHandler(ReciboCompraIncorrectaException.class)
+	    public ResponseEntity<ErrorResponse> handleEventoNotFoundException(ReciboCompraIncorrectaException ex) {
+	        logger.error("Intento de compra fallido: {}", ex.getMessage());
+	        ErrorResponse errorResponse = new ErrorResponse( HttpStatus.BAD_REQUEST.value(),"Intento de compra fallido",ex.getMessage());
+	        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+	    }
 	    
 
 }
