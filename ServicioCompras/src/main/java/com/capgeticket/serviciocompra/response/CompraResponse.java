d082e6b37192d88c5c450bda9eefda070fa2b632
Guillermo <guillerg@LES-5CG12150S3.corp.capgemini.com>
2024-10-24 12:07:29 +0200

    Commit de CompraAdapter

--- ServicioCompras/src/main/java/com/capgeticket/serviciocompra/response/CompraResponse.java
+++ ServicioCompras/src/main/java/com/capgeticket/serviciocompra/response/CompraResponse.java
@@ -4,14 +4,22 @@ import java.io.Serializable;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
+import com.capgeticket.resteventos.model.Evento;
+import com.capgeticket.serviciocompra.model.Compra;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-@Data
+/**
+ * Clase: CompraResponse
+ * Descripción: clase dto con los datos más relevantes
+ * Fecha: 23/10/24
+ * Versión: 1.0
+ * Autores: Guillermo Garcia
+ */
 @Builder
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class CompraResponse implements Serializable {
