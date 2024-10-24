c1b34ed68cff0ea32f7a349f46bed6e501056921
Gregorio Rosas <laura.gregorio-rosas@capgemini.com>
2024-10-24 13:52:53 +0200

    fix feign

--- ServicioCompras/src/main/java/com/capgeticket/serviciocompra/ServicioComprasApplication.java
+++ ServicioCompras/src/main/java/com/capgeticket/serviciocompra/ServicioComprasApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableFeignClients
+@EnableFeignClients(basePackages = "com.capgeticket.feignclients")
 public class ServicioComprasApplication {
 
 	public static void main(String[] args) {
