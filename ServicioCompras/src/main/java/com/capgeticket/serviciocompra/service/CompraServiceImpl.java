ccb453e69262c0cf500743183767ff2ae46fd371
Cordero Sanchez <laura-nathalia.cordero-sanchez@capgemini.com>
2024-10-25 11:51:20 +0200

    realizados tests

--- ServicioCompras/src/main/java/com/capgeticket/serviciocompra/service/CompraServiceImpl.java
+++ ServicioCompras/src/main/java/com/capgeticket/serviciocompra/service/CompraServiceImpl.java
@@ -141,7 +141,7 @@ public class CompraServiceImpl implements CompraService {
 		} catch (FeignException e) {
 			log.error("Error Feign: " + e.contentUTF8());
 			throw new ReciboCompraIncorrectaException(
-					"Error en la comunicación con el banco: el sistema se encuentra inestable");
+					"Error en la comunicación con el banco: el sistema se encuentra inestable.");
 		}
 	}
 
--- ServicioCompras/src/test/java/com/capgeticket/serviciocompra/NuevaCompraTests.java
+++ ServicioCompras/src/test/java/com/capgeticket/serviciocompra/NuevaCompraTests.java
@@ -1,17 +1,26 @@
 package com.capgeticket.serviciocompra;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.time.LocalDateTime;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
+import com.capgeticket.resteventos.model.Evento;
+import com.capgeticket.serviciocompra.response.DatosCompraResponse;
 import com.capgeticket.serviciocompra.response.PeticionCompraResponse;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
+
 
 import jakarta.ws.rs.core.MediaType;
 
@@ -26,25 +35,119 @@ import jakarta.ws.rs.core.MediaType;
 class NuevaCompraTests {
 
 	@Autowired
-	private MockMvc mockMvc;
+    private MockMvc mockMvc;
 
-	@Autowired
-	private ObjectMapper objectMapper;
+    @Autowired
+    private ObjectMapper objectMapper;
 
-	@Test
-	public void shouldSuccessfullyAddCompra() throws Exception {
-		PeticionCompraResponse peticionCompraResponse = new PeticionCompraResponse();
-		peticionCompraResponse.setEmail("usuario@example.com");
-		peticionCompraResponse.setIdEvento(6L);
-		peticionCompraResponse.setNumeroTarjeta("4444-4444-4444-4444r");
-		peticionCompraResponse.setMesCaducidad(12);
-		peticionCompraResponse.setYearCaducidad(2025);
-		peticionCompraResponse.setCvv(123);
-		peticionCompraResponse.setEmisor("VISA");
+    PeticionCompraResponse peticionCompra;
+    
+    DatosCompraResponse datosCompra;
 
-		String peticionCompraJson = objectMapper.writeValueAsString(peticionCompraResponse);
+    @BeforeEach
+    public void setUp() {
+        peticionCompra = new PeticionCompraResponse();
+        peticionCompra.setEmail("usuario@example.com");
+        peticionCompra.setIdEvento(6L);
+        peticionCompra.setNumeroTarjeta("4444-4444-4444-4444");
+        peticionCompra.setMesCaducidad(12);
+        peticionCompra.setYearCaducidad(2025);
+        peticionCompra.setCvv(123);
+        peticionCompra.setEmisor("VISA");
+    }
 
-		mockMvc.perform(post("/nuevaCompra").contentType(MediaType.APPLICATION_JSON).content(peticionCompraJson))
-				.andExpect(status().isCreated());
-	}
+    @Test
+    public void shouldSuccessfullyAddCompra() throws Exception {
+        String peticionCompraJson = objectMapper.writeValueAsString(peticionCompra);
+
+        mockMvc.perform(post("/nuevaCompra")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(peticionCompraJson))
+                .andExpect(status().isCreated());
+    }
+
+    @Test
+    public void shouldReturnException_InvalidEmail() throws Exception {
+        peticionCompra.setEmail("usuarioexample"); 
+
+        String peticionCompraJson = objectMapper.writeValueAsString(peticionCompra);
+
+        mockMvc.perform(post("/nuevaCompra")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(peticionCompraJson))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.error").value("La solicitud es incorrecta. Verifica los datos enviados."))
+                .andExpect(jsonPath("$.details").value("El email tiene un formato incorrecto."));
+    }
+
+    @Test
+    public void shouldReturnException_InvalidEvento() throws Exception {
+        peticionCompra.setIdEvento(0L); 
+        
+        String peticionCompraJson = objectMapper.writeValueAsString(peticionCompra);
+
+        mockMvc.perform(post("/nuevaCompra")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(peticionCompraJson))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.error").value("La solicitud es incorrecta. Verifica los datos enviados."))
+                .andExpect(jsonPath("$.details").value("El id del evento no puede ser 0."));
+    }
+    
+    @Test
+    public void shouldReturnException_InvalidNumeroTarjeta() throws Exception {
+        peticionCompra.setNumeroTarjeta("1234123412341234");;
+        
+        String peticionCompraJson = objectMapper.writeValueAsString(peticionCompra);
+
+        mockMvc.perform(post("/nuevaCompra")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(peticionCompraJson))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.error").value("Intento de compra fallido."))
+                .andExpect(jsonPath("$.details").value("Error al realizar la compra: El número de la tarjeta no es válido."));
+    }
+    
+    @Test
+    public void shouldReturnException_InvalidMesCaducidad() throws Exception {
+        peticionCompra.setMesCaducidad(25);
+        
+        String peticionCompraJson = objectMapper.writeValueAsString(peticionCompra);
+
+        mockMvc.perform(post("/nuevaCompra")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(peticionCompraJson))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.error").value("Intento de compra fallido."))
+                .andExpect(jsonPath("$.details").value("Error al realizar la compra: El mes de caducidad no es correcto."));
+    }
+    
+    @Test
+    public void shouldReturnException_InvalidYearCaducidad() throws Exception {
+        peticionCompra.setYearCaducidad(1999);
+        
+        String peticionCompraJson = objectMapper.writeValueAsString(peticionCompra);
+
+        mockMvc.perform(post("/nuevaCompra")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(peticionCompraJson))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.error").value("Intento de compra fallido."))
+                .andExpect(jsonPath("$.details").value("Error al realizar la compra: El año de caducidad no es correcto."));
+    }
+    
+    @Test
+    public void shouldReturnException_InvalidCvv() throws Exception {
+        peticionCompra.setCvv(111111);
+        
+        String peticionCompraJson = objectMapper.writeValueAsString(peticionCompra);
+
+        mockMvc.perform(post("/nuevaCompra")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(peticionCompraJson))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.error").value("Intento de compra fallido."))
+                .andExpect(jsonPath("$.details").value("Error al realizar la compra: El formato del CVV no es válido."));
+    }
+    
 }
