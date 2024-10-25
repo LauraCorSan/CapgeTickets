1f71a9b4202e6178ee62c2a538e22204db5d8c22
Guillermo <guillerg@LES-5CG12150S3.corp.capgemini.com>
2024-10-25 10:06:05 +0200

    Datos writter,Procesor, Reader

--- ServicioCompras/src/main/java/com/capgeticket/job/EstadisticasItemWriter.java
+++ ServicioCompras/src/main/java/com/capgeticket/job/EstadisticasItemWriter.java
@@ -1,5 +1,7 @@
 package com.capgeticket.job;
 
+import java.util.List;
+
 import org.springframework.batch.item.Chunk;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,25 +9,25 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
 import com.capgeticket.serviciocompra.model.EstadisticasCompra;
+import com.capgeticket.serviciocompra.repository.EstadisticasRepository;
 
-/*
 @Component
 public class EstadisticasItemWriter implements ItemWriter<EstadisticasCompra> {
 
     @Autowired
-    private EstadisticasCompraRepository estadisticasCompraRepository;
+    private EstadisticasRepository estadisticasRepository;
 
     /**
      * Método write que guarda la lista de EstadisticasCompra en la base de datos.
-     
+     */
     @Override
-    public void write(List<? extends EstadisticasCompra> list) throws Exception {
+    public void write(Chunk<? extends EstadisticasCompra> chunk) throws Exception {
+        List<? extends EstadisticasCompra> list = chunk.getItems(); // Obtener los ítems del chunk
         if (list != null && !list.isEmpty()) {
-            estadisticasCompraRepository.saveAll(list);
+            estadisticasRepository.saveAll(list); // Utiliza el EstadisticasRepository para guardar
         } else {
             // Manejo de caso vacío
             System.out.println("No hay estadísticas para guardar.");
         }
     }
 }
-*/
